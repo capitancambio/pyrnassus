7520343db2d5b3c7d47a64b0fe3126e472277160
capitancambio <capitan.cambio@gmail.com>
2015-05-27 18:13:40 +0100

    Inital commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import pyrnassus
+setup(
+    name = "pyrnassus",
+    version = "0.1",
+    packages = ["pyrnassus"],
+    author = "Javier Asensio-Cubero",
+    author_email = "capitan.cambio@gmail.com",
+    description = "muse-io 3.6.x compatible server",
+    license = "MIT",
+    keywords = "muse headband,muse-io",
+    install_requires=[
+            "pyliblo>=0.9.1",
+            ],
+
+)
